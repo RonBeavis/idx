49e85fa7577a694e8428130908b25d3caf3908ad
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2019-08-11 18:15:53 +0000

    Add files via upload

--- reportX.py
+++ reportX.py
@@ -2,11 +2,8 @@
 # Copyright © 2019 Ronald C. Beavis
 # Licensed under Apache License, Version 2.0, January 2004
 #
-
+# Creates an output file from an idX job
 #
-# Identifies kernels corresponding to spectra
-#
-# Reads kernels in either plain text or gzip'd text
 # idX version 2019.08.10.01
 #
 
@@ -18,9 +15,15 @@ import gzip
 import sys
 import itertools
 import copy
+
+#
+# formats the output of an idX job into a TSV file
+#
+
 def report_ids(_ids,_p):
 	sdict = {}
 	sv = {}
+	# create an index of the results in _ids
 	for j in _ids:
 		sv[j['sn']] = j
 		for k in j['kernels']:
@@ -28,6 +31,7 @@ def report_ids(_ids,_p):
 				sdict[k].add(j['sn'])
 			else:
 				sdict[k] = {j['sn']}
+	# load the kernel file corresponding to the results
 	fname = _p['kernel file']
 	odict = {}
 	if fname.find('.gz') == len(fname) - 3:
@@ -35,17 +39,22 @@ def report_ids(_ids,_p):
 	else:
 		f = open(fname,'r', encoding='utf-8')
 	sub = dict()
+	#read through the kernel file and extract required information
 	for i,l in enumerate(f):
+		# check to see if line has information in _ids
 		if i not in sdict:
 			continue
 		js = ujson.loads(l)
-		if 'pm' not in js:
-			continue
 		spectra = sdict[i]
 		last_i = 0
+		# create lines of output information
+		# and store in odict
 		for s in spectra:
 			delta = (sv[s]['pm']-js['pm'])/1000.0
 			ppm = 1.0e6*(sv[s]['pm']-js['pm'])/js['pm']
+			# update the "sub" identifier for an identification
+			# used to distiguish ids when there is more than
+			# one kernel identified for a particular spectrum
 			if s+1 in sub:
 				sub[s+1] += 1
 			else:
@@ -60,9 +69,12 @@ def report_ids(_ids,_p):
 				odict[s] = [oline]
 			
 	f.close()
+	# open the output file specified in _p
 	o = open(_p['output file'],'w')
+	# create the header line
 	oline = 'Id\tScan\tPeptide mass\tDelta\tppm\tz\tProtein acc\tStart\tEnd\tSequence\tScore\tRI\tFreq'
 	o.write(oline + '\n')
+	# output the lines in odict, sorted by id number
 	for a in sorted(odict):
 		for t in odict[a]:
 			o.write(t + '\n')
