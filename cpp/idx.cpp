fecadb6063f3e80b7cf1355a278ec7291dd4cb4e
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2019-11-12 17:52:50 +0000

    Add files via upload

---
+++ cpp/idx.cpp
@@ -0,0 +1,102 @@
+/*
+ Copyright © 2019 Ronald C Beavis, all rights reserved
+*/
+
+#include "nlohmann/json.hpp"
+#include <iostream>
+#include <cstdio>
+#include <sys/stat.h>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <ctime>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+#include "load_spectra.hpp"
+
+
+
+// for convenience
+int main(int argc, char* argv[])	{
+	if(argc < 4)	{
+		cout << "usage:\t>idX SPECTRA_FILE KERNEL_FILE OUTPUT_FILE (high|medium|low*)\n";
+		return 0;
+	}
+	map<string,string> params;
+	vector<spectrum> spectra;
+	using json = nlohmann::json;
+	string version = "idX, 2020.1";
+	params["version"] = version;
+
+	long fragment_tolerance = 400;
+	if(argc > 4 and strcmp(argv[4],"high") == 0)	{
+		fragment_tolerance = 20;
+	}
+	else if (argc > 4 and strcmp(argv[4],"medium") == 0)	{
+		fragment_tolerance = 50;
+	}
+	char ptemp[64];
+	sprintf(ptemp,"%li",fragment_tolerance);
+	params["fragment tolerance"] = ptemp;
+
+	string spectrum_file = argv[1];
+	struct stat fbuffer;
+    	if(stat(spectrum_file.c_str(), &fbuffer) != 0)	{
+		cout << "Error (idx:0001): spectrum file \"" << spectrum_file << "\" does not exist\n";
+		return 0;
+	}
+	params["spectrum file"] = spectrum_file;
+
+	string kernel_file = argv[2];
+    	if(stat(kernel_file.c_str(), &fbuffer) != 0)	{
+		cout << "Error (idx:0002): kernel file \"" << kernel_file << "\" does not exist\n";
+		return 0;
+	}
+	params["kernel file"] = kernel_file;
+
+	string output_file = argv[3];
+	params["output file"] = output_file;
+
+	long maximum_spectra = -1;
+	if(argc > 5)	{
+		maximum_spectra = atoi(argv[5]);
+	}
+	sprintf(ptemp,"%li",maximum_spectra);
+	params["maximum spectra"] = ptemp;
+
+	long parent_tolerance = 20;
+	sprintf(ptemp,"%li",parent_tolerance);
+	params["parent_tolerance"] = ptemp;
+
+	cout << "\nstart ...\nidX parameters" << "\n";
+	if(maximum_spectra != -1)	{
+		cout << "\t   max spectra: " << maximum_spectra << "\n";
+	}
+	else	{
+		cout << "\t   max spectra: unlimited" << "\n";
+	}
+	cout << "\t  fragment tol: " << fragment_tolerance << " mDa\n";
+	cout << "\t    parent tol: " << parent_tolerance << " ppm\n";
+	cout << "\t spectrum file: " << spectrum_file << "\n";
+	cout << "\t   kernel file: " << kernel_file << "\n";
+	cout << "\t   output file: " << output_file << "\n";
+	cout << "\t       version: " << version << "\n";
+	cout << "load & index spectra"  << "\n";
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	load_spectra ls;
+	if(!ls.load(params,spectra))	{
+		cout << "Error (idx:0003): failed to load spectrum file \"" << spectrum_file << "\"\n";
+		return 0;
+	}
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	cout << "spectra: " << spectra.size() << "\n";
+	cout << "elapsed time: " << duration_cast<milliseconds>(t2 - t1).count() << " ms\n";
+	return 0;
+//	auto j3 = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
+//	cout << j3.dump(1) << "\n";
+//	cout << j3["happy"] << "\n";
+//	cout << j3["pi"] << "\n";
+}
+
---
+++ cpp/load_spectra.hpp
@@ -0,0 +1,130 @@
+/*
+ Copyright © 2019 Ronald C Beavis, all rights reserved
+*/
+
+#include <algorithm>
+
+class spectrum
+{
+public:
+	spectrum(void)	{pm = 0;pi=0;pz=0;sc=0;}
+	virtual ~spectrum(void)	{ clear(); }
+	long pm;
+	long pi;
+	long pz;
+	long sc;
+	long isum;
+	vector<pair<long,long>> mis;
+	string desc;
+	bool clear()	{sc = 0;pm = 0; pi = 0; pz = 0; sc=0; desc = ""; mis.clear();}
+	spectrum& operator=(const spectrum &rhs)	{
+		mis.clear();
+		pm = rhs.pm;
+		pi = rhs.pi;
+		pz = rhs.pz;
+		desc = rhs.desc;
+		sc = rhs.sc;
+		pair<long,long> p;
+		for(size_t i = 0; i < rhs.mis.size(); i++)	{
+			p.first = rhs.mis[i].first;
+			p.second = rhs.mis[i].second;
+			mis.push_back(p);
+		}
+	}
+	bool condition(long _ires, long _l)	{
+		long a = 0;
+		double i_max = 0.0;
+		long m = 0;
+		long i = 0;
+		for(size_t a = 0; a < mis.size(); a++)	{
+			m = mis[a].first;
+			i = mis[a].second;		
+			if(m < 150000 or (long)fabs(pm-m) < 45000 or (long)fabs(pm/pz- m) < 2000)	{
+				continue;
+			}
+			if(i > i_max)	{
+				i_max = i;
+			}
+		}
+		vector<long> sMs;
+		vector<long> sIs;
+		i_max = i_max/100.0;
+		for(size_t a = 0; a < mis.size(); a++)	{
+			m = mis[a].first;
+			if(m < 150000 or (long)fabs(pm-m) < 45000 or (long)fabs(pm/pz-m) < 2000)	{
+				continue;
+			}
+			if((float)(mis[a].second)/i_max > 1.00)	{
+				i = (long)(mis[a].second/i_max);
+				if(!sMs.empty())	{
+					if((long)fabs(sMs.back() - m) < 500)	{
+						if(sIs.back() < i)	{
+							sMs.pop_back();
+							sIs.pop_back();
+							sMs.push_back(m);
+							sIs.push_back(i);
+						}
+					}
+					else	{
+						sMs.push_back(m);
+						sIs.push_back(i);
+					}
+				}
+				else	{
+					sMs.push_back(m);
+					sIs.push_back(i);
+				}
+			}
+		}
+		vector<pair<long,long> > pMs;
+		pair<long,long> p(0,0);
+		for(size_t a = 0; a < sMs.size();a++)	{
+			p.first = sMs[a];
+			p.second = sIs[a];
+			pMs.push_back(p);
+		}
+
+		sort(pMs.begin(), pMs.end(), 
+               		[](const auto& x, const auto& y) { return x.second < y.second; } );
+
+		long max_l = 2 * (long)((0.5 + (float)pm)/100000.0);
+		if(max_l > _l)	{
+			max_l = _l;
+		}
+		while(pMs.size() > max_l)	{
+			pMs.pop_back();
+		}
+		sort(pMs.begin(), pMs.end(), 
+               		[](const auto& x, const auto& y) { return x.first < y.first; } );
+
+//
+//		generate a normalized set of spectrum masses
+//
+		long is = 0;
+		mis.clear();
+		for(size_t a=0; a < pMs.size();a++)	{
+			m = pMs[a].first;
+			p.first = (long)((0.5+(double)m)/_ires);
+			p.second = pMs[a].second;
+			mis.push_back(p);
+			p.first -= 1;
+			mis.push_back(p);
+			p.first += 2;
+			mis.push_back(p);
+			is += 3*p.second;
+		}
+		isum = is;
+		return true;
+	}
+};
+
+class load_spectra
+{
+public:
+	load_spectra(void);
+	virtual ~load_spectra(void);
+	bool load(map<string,string>& _p,vector<spectrum>& _s);
+};
+
+
+
