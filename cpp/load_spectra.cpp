501820364aeaf040717ba95947b93e97c03c6ba9
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2019-11-23 17:55:55 +0000

    Add files via upload

--- cpp/load_spectra.cpp
+++ cpp/load_spectra.cpp
@@ -11,13 +11,12 @@
 #include <fstream>
 #include <cstdio>
 #include <iostream>
-#include <sys/stat.h>
 #include <map>
 #include <string>
 #include <set>
 #include <vector>
-#include "parallel_hashmap/phmap.h"
-using namespace std;
+#include "parallel_hashmap/phmap.h" //fast maps and sets used for indexing
+using namespace std; //namespace used throughout
 #include "load_spectra.hpp"
 
 load_spectra::load_spectra(void)	{
@@ -26,9 +25,13 @@ load_spectra::load_spectra(void)	{
 load_spectra::~load_spectra(void)	{
 }
 
+//
+//	loads spectra using the MGF file specified in _params
+//
+
 bool load_spectra::load(map<string,string>& _params)	{
 	ifstream istr;
-	istr.open(_params["spectrum file"]);
+	istr.open(_params["spectrum file"]); // opens input file stream
 	int64_t res = (int64_t)atoi(_params["fragment tolerance"].c_str());
 	if(istr.fail())	{
 		return false;
@@ -36,55 +39,56 @@ bool load_spectra::load(map<string,string>& _params)	{
 	double pt = atoi(_params["parent tolerance"].c_str());
 	size_t len = 1024;
 	size_t size = 0;
-	char *line = new char[len];
+	char *line = new char[len]; //char buffer used to read file
 	string temp = "";
-	string desc = "";
+	string desc = ""; //description information (if available)
 	char *pos = NULL;
 	size_t equals = 0;
-	double parent = 0.0;
-	double charge = 1.0;
-	string run_time = "";
+	double parent = 0.0; //parent mass
+	double charge = 1.0; //parent charge
+	string run_time = ""; //chromatographic retention time information (if available)
 	vector<double> masses;
 	vector<double> intensities;
 	spectrum sp;
-	const double proton = 1.007276;
-	int64_t scan = 0;
+	const double proton = 1.007276; //constant used to recalculate neutral masses
+	int64_t scan = 0; //scan number of the spectrum
 	int64_t s = 1;
+	// process the file, one line at a time
 	while(istr.good() && !istr.eof())	{
 		istr.getline(line,len-1,'\n');
 		temp = line;
 		size = temp.size();
 		equals = temp.find("=");
-		if(temp.find("PEPMASS=") != temp.npos)	{
+		if(temp.find("PEPMASS=") != temp.npos)	{ //tag corresponding to the parent ion mass
 			temp = temp.substr(equals+1,size-equals+1);
 			parent = atof(temp.c_str());
 		}
-		else if(temp.find("BEGIN IONS") != temp.npos)	{
+		else if(temp.find("BEGIN IONS") != temp.npos)	{ //tag starting a single spectrum
 			desc = "";
 		}
-		else if(temp.find("#") == 0)	{
+		else if(temp.find("#") == 0)	{ //comment line
 			desc += line+1;
 			desc += " ";
 		}
-		else if(temp.find("TITLE=") != temp.npos)	{
+		else if(temp.find("TITLE=") != temp.npos)	{ //tag for the description information
 			desc += temp.substr(equals+1,size-equals+1);
 			desc += " ";
 		}
-		else if(temp.find("RTINSECONDS=") != temp.npos)	{
+		else if(temp.find("RTINSECONDS=") != temp.npos)	{ //tag for retention time information
 			run_time = temp.substr(equals+1,size-equals+1);
 			desc += "RTINSECONDS=";
 			desc += run_time;
 			desc += " ";
 		}
-		else if(temp.find("CHARGE=") != temp.npos)	{
+		else if(temp.find("CHARGE=") != temp.npos)	{ //tag for parent ion charge
 			temp = temp.substr(equals+1,size-equals+1);
 			charge = atof(temp.c_str());
 		}
-		else if(temp.find("SCANS=") != temp.npos)	{
+		else if(temp.find("SCANS=") != temp.npos)	{ //tag for spectrum scan number (if available)
 			temp = temp.substr(equals+1,size-equals+1);
 			scan = atoi(temp.c_str());
 		}
-		else if(atof(line) > 0.0)	{
+		else if(atof(line) > 0.0)	{ //if no tag present, interpret as mass intensity pair
 			masses.push_back(atof(line));
 			pos = line;
 			while(*pos != '\0' && isspace(*pos) != 0)	{
@@ -95,7 +99,8 @@ bool load_spectra::load(map<string,string>& _params)	{
 			}
 			intensities.push_back(atof(pos));
 		}
-		else if(temp.find("END IONS") != temp.npos)	{
+		else if(temp.find("END IONS") != temp.npos)	{ //tag for the end of a spectrum
+			//carry out calculations and type conversions
 			sp.clear();
 			if(parent*charge > 600.0)	{
 				sp.pm = (int64_t)(0.5 + 1000*(parent*charge - proton*charge));
@@ -103,6 +108,7 @@ bool load_spectra::load(map<string,string>& _params)	{
 				sp.pi = 100;
 				sp.pt = pt;
 				sp.desc = desc;
+				//substitute ordinal value for scan number, if no scan available
 				if(scan == 0)	{
 					sp.sc = s;
 				}
@@ -127,12 +133,14 @@ bool load_spectra::load(map<string,string>& _params)	{
 					}
 				}
 			}
+			//clean up to be ready for the next spectrum
 			desc = "";
 			masses.clear();
 			intensities.clear();
 			parent = 0.0;
 			charge = 0.0;
 			s++;
+			//output keep-alive text for logging
 			if(s % 2500 == 0)	{
 				cout << '.';
 				cout.flush();
